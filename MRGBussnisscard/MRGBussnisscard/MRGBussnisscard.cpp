7880eff01983aedc596bebab9afec958ad732acf
gank498 <47163817+gank498@users.noreply.github.com>
2019-02-05 19:02:10 -0600

    Update MRGBussnisscard.cpp

--- MRGBussnisscard/MRGBussnisscard/MRGBussnisscard.cpp
+++ MRGBussnisscard/MRGBussnisscard/MRGBussnisscard.cpp
@@ -2,13 +2,13 @@
  Mathew Garcia
  2/5/2019
  version 1.0
- ITSE - 1307 2019
+ ITSE - 1307
 
 
 
 */ 
 
- */
+ {
 #include "pch.h"
 #include <iostream>
 #include <windows.h>
@@ -21,10 +21,11 @@
 using namespace std;
 int main()
 {
-	std::cout << "Ganks's Computer Services\n"; // used "/n" to move to next line of code
+	std::cout << "Ganks's Computer Services\n";
 	cout << "555 Broadaway Ave\n";
 	cout << "broken pc? or running slow\n";
 	cout << "call us now at 210-555-5555\n";
 	cout << "or checkout our website http://www.gankPC.org\n";
 	cout << "info.gankPC.org/";
+	return 0;
 }
