2cd34ffbdba6051906d5c083120950c915eac817
gank498 <47163817+gank498@users.noreply.github.com>
2019-02-05 18:40:46 -0600

    Add files via upload

---
+++ MRGBussnisscard/MRGBussnisscard/MRGBussnisscard.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+	std::cout << "Ganks's Computer Services\n";
+	cout << "555 Broadaway Ave\n";
+	cout << "broken pc? or running slow\n";
+	cout << "call us now at 210-555-5555\n";
+	cout << "or checkout our website http://www.gankPC.org\n";
+	cout << "info.gankPC.org/";
+}
