a2010d7f8201fb7f64806496b930d04dad549732
gank498 <47163817+gank498@users.noreply.github.com>
2019-05-01 16:43:58 -0500

    Add files via upload

---
+++ PhoneBook/Contact.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Contact
+{
+public:
+	Contact();
+	~Contact();
+private:
+	std::string strFirstName;
+	std::string strLastName;
+	std::string strPhoneNumber;
+public:
+	void setFirstName(std::string strpFirstName);
+	std::string getFirstName();
+	void inputFirstName();
+	void setLastName(std::string strpLastName);
+	std::string getLastName();
+	void inputLastName();
+	void input();
+	void setPhoneNumber(std::string strpPhoneNumber);
+	std::string getPhoneNumber();
+	void inputPhoneNumber();
+	std::string toString();
+};
+
